98d02e45417b16de57638d65e262599659f23889
Talia Karasov <talia.karasov@tuebingen.mpg.de>
2019-03-12 11:25:51 +0100

    Comparing Variance within a trait to that between

---
+++ code/variance_same_different.py
@@ -0,0 +1,6 @@
+import matplotlib as
+import numpy as np
+#The goal of this Rscript is to compare variance within vs variance between
+
+conditions = read.table("/ebio/abt6_projects9/tnseq/data/fitness_datasets/all_conditions.txt", header = TRUE, sep = "\t")
+
